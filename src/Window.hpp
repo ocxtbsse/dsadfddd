013e9a07e3b78120becb35a4ac15bde1e0ab8562
ocxtbsse <ocxtbsse@gmail.com>
2026-02-07 23:47:39 +0300

    Create Window.hpp

---
+++ src/Window.hpp
@@ -0,0 +1,36 @@
+#pragma  once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+
+class Window{
+    public:
+    GLFWwindow* window;
+
+    int createWindow(int windowWidth,int windowHeight, char* sigmaSwagTitle322){
+        if (!glfwInit()) {
+            std::cerr << "GLFW init failed!" << std::endl;
+            return 1;
+        }
+        
+            // 2. Создание окна
+        window = glfwCreateWindow(windowWidth, windowHeight, sigmaSwagTitle322, NULL, NULL);
+        if (!window) {
+            glfwTerminate();
+            return 1;
+        }
+        glfwMakeContextCurrent(window);
+                // 3. Инициализация GLEW (Строго после контекста!)
+        glewExperimental = GL_TRUE;
+        if (glewInit() != GLEW_OK) {
+            std::cerr << "GLEW init failed!" << std::endl;
+            return 1;
+        }
+
+        return 0;
+    }
+};
+
