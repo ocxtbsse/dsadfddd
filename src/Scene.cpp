6eb90a368edd5e29b3da42a99b72978ac08de7ff
ocxtbsse <ocxtbsse@gmail.com>
2026-02-15 19:13:16 +0300

    Update Scene.cpp

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include <iostream>
 
-extern int meshLoadUlta(GameObject& targetObj,char* modelName); 
+extern int meshLoadUlta(GameObject& targetObj,char* modelName);
+extern unsigned int textureLoad(const char* path);
 std::vector<GameObject> sceneObjects;
 std::vector<Camera> sceneCamers;
 
-int startSceneCreateOnjects(){//лень менять название
+int startSceneCreateOnjects(){
     Camera cam;
     cam.position = glm::vec3(0.0f, 0.0f, -2.0f);
     cam.front = glm::vec3(0.0f, 0.0f, -1.0f);
@@ -16,20 +17,50 @@ int startSceneCreateOnjects(){//лень менять название
 
 
     GameObject cube;
-    cube.mesh.modelName = "model.obj";
+    cube.mesh.modelName = "models/model2.obj";
     meshLoadUlta(cube,cube.mesh.modelName);
-    cube.position = glm::vec3(0.0f,0.0f,-2.0f);
-    cube.rotation = glm::vec3(0.0f, 0.0f, 0.0f); 
+
+
+    cube.material.textureName = "textures/treeT.jpg";
+    cube.material.textureID = textureLoad(cube.material.textureName);
+
+    cube.position = glm::vec3(0.0f,-4.0f,-2.0f);
+    cube.rotation = glm::vec3(-90.0f, 0.0f, 0.0f); 
     cube.scale = glm::vec3(10.0f,10.0f,10.0f);
     pushBackObject(cube);
 
 
+
+
+    GameObject cube3;
+    cube3.mesh.modelName = "models/model.obj";
+    meshLoadUlta(cube3,cube3.mesh.modelName);
+
+
+    cube3.material.textureName = "textures/listva.jpg";
+    cube3.material.textureID = textureLoad(cube3.material.textureName);
+
+    cube3.position = glm::vec3(0.0f,-4.0f,-2.0f);
+    cube3.rotation = glm::vec3(0.0f, 0.0f, 0.0f); 
+    cube3.scale = glm::vec3(10.0f,10.0f,10.0f);
+    pushBackObject(cube3);
+
+
+
+
+
     GameObject cube2;
-    cube2.mesh.modelName = "model.obj";
+    cube2.mesh.modelName = "models/cube.obj";
     meshLoadUlta(cube2,cube2.mesh.modelName);
-    cube2.position = glm::vec3(0.0f,0.0f,-2.0f);
+
+    
+    cube2.material.textureName = "textures/wall.jpg";
+    cube2.material.textureID = textureLoad(cube2.material.textureName);
+
+
+    cube2.position = glm::vec3(0.0f,-5.0f,-2.0f);
     cube2.rotation = glm::vec3(0.0f, 0.0f, 0.0f); 
-    cube2.scale = glm::vec3(10.0f,10.0f,10.0f);
+    cube2.scale = glm::vec3(100.0f,1.0f,100.0f);
     pushBackObject(cube2);
 
 
@@ -50,12 +81,12 @@ int startSceneCreateOnjects(){//лень менять название
 
 
 
-void pushBackObject(GameObject PtrGMO){//лень менять название
+void pushBackObject(GameObject PtrGMO){
     sceneObjects.push_back(PtrGMO);    
 }
 
 
-void pushBackCamera(Camera PtrCam){//лень менять название
+void pushBackCamera(Camera PtrCam){
     sceneCamers.push_back(PtrCam);
 }
 
