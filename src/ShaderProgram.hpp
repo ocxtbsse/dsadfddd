702f89ec6d67280ca689096e697732e8bb0b7960
ocxtbsse <ocxtbsse@gmail.com>
2026-02-18 22:54:24 +0300

    Update ShaderProgram.hpp

--- src/ShaderProgram.hpp
+++ src/ShaderProgram.hpp
@@ -2,6 +2,7 @@
 #include <GL/glew.h>
 #include <GLM/glm.hpp>
 #include <GLM/gtc/type_ptr.hpp>
+#include "Matrix.h"
 
 
 class ShaderProgram{
@@ -32,12 +33,17 @@ class ShaderProgram{
     } 
 
 
-    void setMat4(const char* name, glm::mat4 matrix) {
+    void setMat4(const char* name, glm::mat4 matrix) {// неведомо зачем
         glUniformMatrix4fv(glGetUniformLocation(this->ID, name), 1, GL_FALSE, glm::value_ptr(matrix));
     }
 
 
-    void setVec3(const std::string &name, const glm::vec3 &value) const {
+    void setMat4x4(const char* name, Matrix mat){
+        glUniformMatrix4fv(glGetUniformLocation(this->ID, name), 1, GL_FALSE, mat.matrix4x4);
+    }
+
+
+    void setVec3(const std::string &name, const glm::vec3 &value) const {//скоро устранится
         glUniform3fv(glGetUniformLocation(this->ID, name.c_str()), 1, &value[0]);
     }
 
